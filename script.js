6ec327c487e755eb79718de2e983772fdf8fd463
Aniket Gala <aniket.gala@gmail.com>
2025-10-04 22:11:33 +0530

    First commit

---
+++ script.js
@@ -0,0 +1,304 @@
+{
+  "questions"; [
+    {
+      "question": "What's the most honest thing a developer can say about their CSS?",
+      "options": [
+        "I know exactly why it works",
+        "I have no idea why it works, but it does",
+        "It's perfectly organized and maintainable",
+        "I never copy-paste from Stack Overflow"
+      ],
+      "correct": "I have no idea why it works, but it does"
+    },
+    {
+      "question": "Why don't HTML developers ever get locked out?",
+      "options": [
+        "They always use strong passwords",
+        "They know all the escape sequences",
+        "They always have the right <key>",
+        "They never close their <door>"
+      ],
+      "correct": "They never close their <door>"
+    },
+    {
+      "question": "What's the developer's favorite way to center a div?",
+      "options": [
+        "display: flex; justify-content: center; align-items: center;",
+        "Pray to the CSS gods and use margin: auto;",
+        "Google it for the 847th time",
+        "All of the above"
+      ],
+      "correct": "All of the above"
+    },
+    {
+      "question": "What happens when you fix one bug in JavaScript?",
+      "options": [
+        "Your code becomes perfect",
+        "Two more bugs appear",
+        "You become a senior developer",
+        "The universe achieves balance"
+      ],
+      "correct": "Two more bugs appear"
+    },
+    {
+      "question": "How do you comfort a JavaScript bug?",
+      "options": [
+        "console.log('There, there')",
+        "Try/catch it in a hug",
+        "Promise it will be resolved",
+        "Tell it undefined is not a function of you"
+      ],
+      "correct": "console.log('There, there')"
+    },
+    {
+      "question": "What's the difference between Java and JavaScript?",
+      "options": [
+        "Java is to JavaScript as Car is to Carpet",
+        "JavaScript is Java's younger, cooler sibling",
+        "Java requires more coffee to understand",
+        "There's no difference, it's all the same"
+      ],
+      "correct": "Java is to JavaScript as Car is to Carpet"
+    },
+    {
+      "question": "Why did the CSS developer break up with HTML?",
+      "options": [
+        "They had no class",
+        "There was no chemistry",
+        "They couldn't align their relationship",
+        "HTML was too semantic for them"
+      ],
+      "correct": "They couldn't align their relationship"
+    },
+    {
+      "question": "What's a developer's favorite type of party?",
+      "options": [
+        "A function call",
+        "An event handler",
+        "A callback party",
+        "A block party (with lots of curly braces)"
+      ],
+      "correct": "A callback party"
+    },
+    {
+      "question": "How many programmers does it take to change a light bulb?",
+      "options": [
+        "None, that's a hardware problem",
+        "One, but they'll rewrite the entire lighting system",
+        "404: Light bulb not found",
+        "It depends on the framework"
+      ],
+      "correct": "None, that's a hardware problem"
+    },
+    {
+      "question": "What do you call a programmer who doesn't comment their code?",
+      "options": [
+        "A mystery writer",
+        "Future enemy of themselves",
+        "Job security specialist",
+        "All of the above"
+      ],
+      "correct": "All of the above"
+    },
+    {
+      "question": "Why do React developers always seem so happy?",
+      "options": [
+        "They love their components",
+        "They're always in a good state",
+        "They know how to handle props-erly",
+        "They've mastered the art of hooks"
+      ],
+      "correct": "They're always in a good state"
+    },
+    {
+      "question": "What's the most terrifying thing in web development?",
+      "options": [
+        "A deadline",
+        "Internet Explorer support",
+        "A client saying 'Can you make it pop more?'",
+        "Merging branches on Friday afternoon"
+      ],
+      "correct": "Merging branches on Friday afternoon"
+    },
+    {
+      "question": "How do you explain APIs to a non-technical person?",
+      "options": [
+        "It's like a waiter at a restaurant",
+        "It's magic, don't question it",
+        "It's the internet's way of gossiping",
+        "It's when computers have a polite conversation"
+      ],
+      "correct": "It's like a waiter at a restaurant"
+    },
+    {
+      "question": "What's the developer's response to 'It works on my machine'?",
+      "options": [
+        "Then we'll ship your machine",
+        "Have you tried turning it off and on again?",
+        "Must be a user error",
+        "Let's dockerize your machine"
+      ],
+      "correct": "Then we'll ship your machine"
+    },
+    {
+      "question": "Why don't developers trust stairs?",
+      "options": [
+        "They prefer elevators (lifts)",
+        "They're always up to something",
+        "Because they're always debugging steps",
+        "They know about infinite loops"
+      ],
+      "correct": "Because they're always debugging steps"
+    },
+    {
+      "question": "What's the first rule of web development?",
+      "options": [
+        "Always blame the browser",
+        "It's not a bug, it's a feature",
+        "Test in production",
+        "Never trust user input"
+      ],
+      "correct": "Never trust user input"
+    },
+    {
+      "question": "How does a developer fix a relationship?",
+      "options": [
+        "Try to catch the exception",
+        "Refactor the entire relationship",
+        "Add more debugging statements",
+        "Check if it's undefined"
+      ],
+      "correct": "Try to catch the exception"
+    },
+    {
+      "question": "What's the most used JavaScript method?",
+      "options": [
+        "console.log()",
+        "stackoverflow.copy()",
+        "panicMode.activate()",
+        "hopeItWorks()"
+      ],
+      "correct": "console.log()"
+    },
+    {
+      "question": "Why did the developer quit their job?",
+      "options": [
+        "They didn't get arrays",
+        "The company had no class",
+        "They couldn't handle the pressure",
+        "Their boss was null and void"
+      ],
+      "correct": "They didn't get arrays"
+    },
+    {
+      "question": "What's the developer's favorite exercise?",
+      "options": [
+        "Git push-ups",
+        "Flexbox yoga",
+        "Runtime running",
+        "Debugging squats"
+      ],
+      "correct": "Git push-ups"
+    },
+    {
+      "question": "How do you know if someone is a frontend developer?",
+      "options": [
+        "They'll tell you within 5 minutes",
+        "They complain about IE while using Chrome",
+        "They have strong opinions about semicolons",
+        "They measure everything in pixels and ems"
+      ],
+      "correct": "They have strong opinions about semicolons"
+    },
+    {
+      "question": "What's the backend developer's motto?",
+      "options": [
+        "Make it work, then make it pretty",
+        "If you can see it, you're doing it wrong",
+        "200 OK is all that matters",
+        "The database knows best"
+      ],
+      "correct": "If you can see it, you're doing it wrong"
+    },
+    {
+      "question": "Why do developers prefer dark mode?",
+      "options": [
+        "It's easier on the eyes during 3 AM coding sessions",
+        "Light attracts bugs",
+        "They're secretly vampires",
+        "White background costs more electricity"
+      ],
+      "correct": "Light attracts bugs"
+    },
+    {
+      "question": "What's a developer's favorite type of music?",
+      "options": [
+        "Heavy metal (because of all the frameworks)",
+        "Hip-hop (for all the loops)",
+        "Jazz (it's all about improvisation)",
+        "Classical (everything has good composition)"
+      ],
+      "correct": "Jazz (it's all about improvisation)"
+    },
+    {
+      "question": "How many developers does it take to screw in a light bulb?",
+      "options": [
+        "None, they just redefine darkness as the new standard",
+        "One, but they'll create a framework for it first",
+        "Two, one to hold the bulb and one to rotate the house",
+        "That's a legacy lighting system, we need to rewrite it"
+      ],
+      "correct": "None, they just redefine darkness as the new standard"
+    },
+    {
+      "question": "What's the developer's favorite day of the week?",
+      "options": [
+        "Friday (deployment day, what could go wrong?)",
+        "Monday (fresh bugs to fix)",
+        "Saturday (no meetings, just code)",
+        "Any day ending in segmentation fault"
+      ],
+      "correct": "Friday (deployment day, what could go wrong?)"
+    },
+    {
+      "question": "Why don't developers ever get lost?",
+      "options": [
+        "They always know their current directory",
+        "They have GPS (Git Position System)",
+        "They follow the breadcrumbs in their code",
+        "They can always find their way home (directory)"
+      ],
+      "correct": "They always know their current directory"
+    },
+    {
+      "question": "What's the most romantic thing a developer can say?",
+      "options": [
+        "You're the only one in my git log",
+        "You make my heart race condition",
+        "I'd never throw an exception with you",
+        "You're my favorite dependency"
+      ],
+      "correct": "You're the only one in my git log"
+    },
+    {
+      "question": "How do developers stay fit?",
+      "options": [
+        "By running npm install",
+        "Doing daily scrums",
+        "Lifting heavy databases",
+        "Sprint planning"
+      ],
+      "correct": "Doing daily scrums"
+    },
+    {
+      "question": "What's the developer's last words before going live?",
+      "options": [
+        "What could possibly go wrong?",
+        "I tested it thoroughly on localhost",
+        "The staging environment is identical to production",
+        "I'm sure nobody will notice that bug"
+      ],
+      "correct": "What could possibly go wrong?"
+    }
+  ]
+}
